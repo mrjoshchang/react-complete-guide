f71ebeea4f3e47ea2a7f5f14328d1b68649245b0
mrjoshchang <joshchang9009@gmail.com>
2023-06-19 14:54:20 -0700

    finish section 3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import ExpenseItem from './components/ExpenseItem';
+import React from 'react';
+import Expenses from './components/Expenses/Expenses';
 
-function App() {
+const App = () => {
   const expenses = [
     { title: 'Car Insurance', amount: 432.29, date: new Date(2023, 5, 18) },
     { title: 'Car tires', amount: 1243.29, date: new Date(2023, 5, 18) },
@@ -10,29 +11,10 @@ function App() {
 
   return (
     <div>
-      <h1>Fuck off!</h1>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      ></ExpenseItem>
+      <h1>Learning React!</h1>
+      <Expenses expenses={expenses} />
     </div>
   );
-}
+};
 
 export default App;
