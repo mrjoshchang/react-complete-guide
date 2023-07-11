5481cd08ec33ea53e08205cf007b91c186ef765e
mrjoshchang <joshchang9009@gmail.com>
2023-07-10 18:25:02 -0700

    checkpoint lesson 123

---
+++ prj-add-users/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import UsersList from './components/UsersList/UsersList';
+import AddUser from './components/AddUser/AddUser';
+
+function App() {
+  return (
+    <div>
+      <AddUser />
+      <UsersList />
+    </div>
+  );
+}
+
+export default App;
