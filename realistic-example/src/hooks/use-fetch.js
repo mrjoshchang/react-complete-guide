92fbe969c9bce70e4ddde5b49f51d48be37d31d1
mrjoshchang <joshchang9009@gmail.com>
2023-07-31 22:26:48 -0700

    fix section 16 issue

--- realistic-example/src/hooks/use-fetch.js
+++ realistic-example/src/hooks/use-fetch.js
@@ -4,34 +4,33 @@ const useFetch = (applyData) => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const sendRequest = useCallback(
-    async (requestConfig) => {
-      setIsLoading(true);
-      setError(null);
-      try {
-        const response = await fetch(requestConfig.url, {
-          method: requestConfig.method ? requestConfig.method : 'GET',
-          headers: requestConfig.headers ? requestConfig.headers : {},
-          body: JSON.stringify(requestConfig.body)
-            ? JSON.stringify(requestConfig.body)
-            : null,
-        });
+  const sendRequest = useCallback(async (requestConfig, applyData) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(requestConfig.url, {
+        method: requestConfig.method ? requestConfig.method : 'GET',
+        headers: requestConfig.headers ? requestConfig.headers : {},
+        body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
+      });
 
-        if (!response.ok) {
-          throw new Error('Request failed!');
-        }
-
-        const data = await response.json();
-        applyData(data);
-      } catch (err) {
-        setError(err.message || 'Something went wrong!');
+      if (!response.ok) {
+        throw new Error('Request failed!');
       }
-      setIsLoading(false);
-    },
-    [applyData]
-  );
 
-  return { isLoading: isLoading, error: error, sendRequest: sendRequest };
+      const data = await response.json();
+      applyData(data);
+    } catch (err) {
+      setError(err.message || 'Something went wrong!');
+    }
+    setIsLoading(false);
+  }, []);
+
+  return {
+    isLoading,
+    error,
+    sendRequest,
+  };
 };
 
 export default useFetch;
