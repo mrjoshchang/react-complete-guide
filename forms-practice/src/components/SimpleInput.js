d2f6f81b9dc9b8f03b39bb3e0887232a8b559df2
Joshua Chang <jchang@Joshuas-MacBook-Pro.local>
2023-08-02 18:53:54 -0700

    finished lesson 238

---
+++ forms-practice/src/components/SimpleInput.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState('')
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false)
+
+  const enteredNameIsValid = enteredName.trim() !== '';
+  const nameInputIsValid = !enteredNameIsValid && enteredNameTouched;
+
+  const nameInputChangeHandler = event => {
+    setEnteredName(event.target.value)
+  }
+
+  const nameInputBlurHandler = event => {
+    setEnteredNameTouched(true)
+  }
+
+  const formSubmissionHandler = event => {
+    event.preventDefault();
+
+    if (!enteredNameIsValid) return
+    setEnteredName('')
+    setEnteredNameTouched(false)
+  }
+
+  const nameInputClasses = nameInputIsValid ? 'form-control invalid' : 'form-control';
+
+  return (
+    <form onSubmit={formSubmissionHandler}>
+      <div className={nameInputClasses}>
+        <label htmlFor='name'>Your Name</label>
+        <input type='text' id='name'
+          onChange={nameInputChangeHandler}
+          value={enteredName}
+          onBlur={nameInputBlurHandler} />
+        {nameInputIsValid && <p className='error-text'>Name must not be empty.</p>}
+      </div>
+      <div className="form-actions">
+        <button>Submit</button>
+
+      </div>
+    </form>
+  );
+};
+
+export default SimpleInput;
