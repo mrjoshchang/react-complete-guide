1a760ad915918885f36c0c38ac3f7e05ce417712
mrjoshchang <joshchang9009@gmail.com>
2023-08-27 21:34:48 -0700

    route examples: lesson 320

---
+++ 12-adv-starting-project/frontend/src/App.js
@@ -0,0 +1,71 @@
+// Challenge / Exercise
+
+// 1. Add five new (dummy) page components (content can be simple <h1> elements)
+//    - HomePage
+//    - EventsPage
+//    - EventDetailPage
+//    - NewEventPage
+//    - EditEventPage
+// 2. Add routing & route definitions for these five pages
+//    - / => HomePage
+//    - /events => EventsPage
+//    - /events/<some-id> => EventDetailPage
+//    - /events/new => NewEventPage
+//    - /events/<some-id>/edit => EditEventPage
+// 3. Add a root layout that adds the <MainNavigation> component above all page components
+// 4. Add properly working links to the MainNavigation
+// 5. Ensure that the links in MainNavigation receive an "active" class when active
+// 6. Output a list of dummy events to the EventsPage
+//    Every list item should include a link to the respective EventDetailPage
+// 7. Output the ID of the selected event on the EventDetailPage
+// BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import EditEvent from './pages/EditEvent';
+import EventDetail from './pages/EventDetail';
+import Events from './pages/Events';
+import Home from './pages/Home';
+import NewEvent from './pages/NewEvent';
+import RootLayout from './pages/RootLayout';
+import EventsRootLayout from './pages/EventsRoot';
+
+const routes = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <Home /> },
+      {
+        path: 'events',
+        element: <EventsRootLayout />,
+        children: [
+          {
+            path: '',
+            element: <Events />,
+            loader: async () => {
+              const response = await fetch('http://localhost:8080/events');
+              if (!response.ok) {
+                // ...
+              } else {
+                const resData = await response.json();
+                return resData.events;
+              }
+            },
+          },
+          { path: ':eventId', element: <EventDetail /> },
+          { path: 'new', element: <NewEvent /> },
+          { path: ':eventId/edit', element: <EditEvent /> },
+        ],
+      },
+    ],
+  },
+]);
+function App() {
+  return (
+    <>
+      <RouterProvider router={routes} />
+    </>
+  );
+}
+
+export default App;
---
+++ 12-adv-starting-project/frontend/src/pages/NewEvent.js
@@ -0,0 +1,9 @@
+function NewEventPage() {
+  return (
+    <>
+      <h1 style={{ textAlign: 'center' }}>New Event</h1>
+    </>
+  );
+}
+
+export default NewEventPage;
